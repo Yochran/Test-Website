a77f5f07e7988f1e897083bb1e2868a2eebb7c67
Yochran <71285258+Yochran@users.noreply.github.com>
2021-02-13 13:31:30 -0500

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -5,36 +5,41 @@ document.getElementById("button-1").onclick = () => {
     const topMove = topPos + "%";
 
     setTimeout(() => {
-        document.getElementById("image").style.left = topMove;
-        document.getElementById("image").style.top = topMove;
+        document.getElementById("options").style.left = topMove;
+        document.getElementById("options").style.top = topMove;
         
-        document.getElementById("position-text").innerHTML = "Image Position: " + topMove + ", " + leftMove;
+        document.getElementById("position-text").innerHTML = "Position: " + topMove + ", " + leftMove;
     }, 100);
 }
 
 document.getElementById("button-2").onclick = () => {
-    let scaleValue = Math.floor(Math.random() * 150);
+    let scaleValue = Math.floor(Math.random() * 200);
+
+    if (scaleValue < 45) {
+        scaleValue = Math.round(scaleValue * 2.5);
+    }
+
     const scaleMove = scaleValue + "%";
 
     setTimeout(() => {
-        document.getElementById("image").style.scale = scaleMove;
+        document.getElementById("options").style.scale = scaleMove;
 
-        document.getElementById("scale-text").innerHTML = "Image Scale: " + scaleMove;
+        document.getElementById("scale-text").innerHTML = "Scale: " + scaleMove;
     }, 100);
 }
 
 document.getElementById("reset-button").onclick = () => {
     resetImage();
 
-    document.getElementById("position-text").innerHTML = "Image Position: Default";
-    document.getElementById("scale-text").innerHTML = "Image Scale: Default";
+    document.getElementById("position-text").innerHTML = "Position: Default";
+    document.getElementById("scale-text").innerHTML = "Scale: Default";
 }
 
 function resetImage() {
     setTimeout(() => {
-        document.getElementById("image").style.top = 35 + "%";
-        document.getElementById("image").style.left = 31 + "%";
-        document.getElementById("image").style.scale = 50 + "%";
+        document.getElementById("options").style.top = 25 + "%";
+        document.getElementById("options").style.left = 33 + "%";
+        document.getElementById("options").style.scale = 100 + "%";
     }, 100);
 }
 
